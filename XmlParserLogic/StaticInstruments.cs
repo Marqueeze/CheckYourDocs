67b8587967c5a78c55c685d3153e5ce412517110
Dmitry <naumenko.dmitry19@gmail.com>
2018-09-08 20:26:19 +0300

    Did OFD-filereader and converter to xml

---
+++ XmlParserLogic/StaticInstruments.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace XmlParserLogic
+{
+    static class StaticInstruments
+    {
+        public static string GetFilePath()
+        {
+            OpenFileDialog OFD = new OpenFileDialog();
+            if (OFD.ShowDialog() == DialogResult.OK)
+            {
+                return OFD.FileName;
+            }
+            throw new Exception("No file chosen");
+        }
+
+        public static string ConvertDocToXml(string input_path)
+        {
+            Microsoft.Office.Interop.Word.Application App = new Microsoft.Office.Interop.Word.Application();
+            if (File.Exists(@input_path))
+            {
+                Regex re = new Regex("*.docx?");
+                string save_pass = re.Replace(@input_path, @"xml");
+                Document doc = App.Documents.Open(@input_path);
+                doc.SaveAs(save_pass);
+                doc.Save();
+                doc.Close();
+                App.Quit();
+                return save_pass;
+            }
+            App.Quit();
+            throw new Exception("Wrong file directory");
+        }
+    }
+}
--- xml-parser/Program.cs
+++ xml-parser/Program.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
 using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+using System.Web.RegularExpressions;
+using System.Xml.Schema;
+using Application = System.Windows.Forms.Application;
 
 namespace xml_parser
 {
@@ -10,6 +18,7 @@ namespace xml_parser
     {
         static void Main(string[] args)
         {
+           
         }
     }
 }
